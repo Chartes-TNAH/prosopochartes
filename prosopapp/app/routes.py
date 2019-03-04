56d56426609faa430449af6a0aa255172a0fa2d0
Camille <camille.besse1@gmail.com>
2019-03-04 17:36:54 +0100

    Corrections de bugs d'affichage

--- prosopapp/app/routes.py
+++ prosopapp/app/routes.py
@@ -3,25 +3,24 @@ from app import app
 
 @app.route('/')
 @app.route('/accueil')
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('accueil.html', title='Home', user=user, posts=posts)
+def accueil ():
+    return render_template("templates/pages/accueil.html")
 
 @app.route('/chercheurs')
+def chercheurs():
+    return render_template("templates/pages/chercheurs.html")
+
 @app.route('/recherche')
+def recherche():
+    return render_template("templates/pages/recherche.html")
+
 @app.route('/resultats')
+def resultat():
+    return render_template("templates/pages/resultats.html")
 #cette route correspond à la page qui affichera les notices abrégées des résultats
 # à voir si on choisit de la conserver sous la dénomination résultat où si l'on préfère un nom qui reprend les mots-clés?
-@app.route('/<str:nomchercheur>')
-# idealement nomchercheur est remplacé par le nomprenom du chercheur
+@app.route('/<int:nomchercheur>')
+def nomchercheur():
+    return render_template("templates/pages/accueil.html")
+#idealement nomchercheur est remplacé par le nomprenom du chercheur
 #cette page correspond à la notice complète sur le chercheur
