e35b8e35063ae42b405986b851655333b85025fd
Camille <camille.besse1@gmail.com>
2019-03-12 10:24:27 +0100

    modifications - corrections des relationships pour la relation n-n

--- prosopapp/app/modeles/donnees.py
+++ prosopapp/app/modeles/donnees.py
@@ -5,15 +5,23 @@ from ..app import db
 # Création du modèle selon celui de la base de données prosopochartes.sqlite
 
 # Table d'association nécessaire à la déclaration d'une relation many-to-many avec la table Occupation dans notre db
-avoir_occupation = Table("avoir_occupation", db.Model.metadata, # !!!! CAMILLE PAS SÛRE POUR CE 'db.Model.metadata' !!!!
-    Column("individu.id", Integer, ForeignKey("individu.id")),
-    Column("occupation.id", Integer, ForeignKey("occupation.id"))
-)
+class Avoir_occupation (db.Model):
+    __tablename__ = "avoir_occupation"
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    individu_id = db.Column(db.Integer, db.ForeignKey("individu.id"))
+    occupation_id = db.Column(db.Integer, db.ForeignKey("occupation.id"))
+    occupations = db.relationship("Occupation", back_populates="avoir_occupation")
+    individus = db.relationship("Individu", back_populates="avoir_occupation")
+    #avoir_occupation = Table("avoir_occupation", db.Model.metadata,
+    #Column("individu.id", Integer, ForeignKey("individu.id")),
+    #Column("occupation.id", Integer, ForeignKey("occupation.id"))
 
 # Table correspondant à un.e chercheu.r.se
-# Par souci de simplicité, chaque membre de la table est dans une relation many to one avec les autres tables
-# (dans notre base, un.e chercheu.r.se n'a qu'un diplôme, une distinction...)
-# Les relations sont donc identifiées par des clefs étrangères
+# Par souci de simplicité, chaque membre de la table est dans la plupart des cas dans une relation many to one avec les autres tables
+# (dans notre base, un.e chercheu.r.se n'a qu'un diplôme, une thèse...)
+#Sauf pour le cas de l'occupation, où nous avons créés une relation many to many et qui suppose l'ajout d'une table
+#supplémentaire que nous avons dénommées avoir_occupation dans notre modèle sqlite.
+# Les relations sont identifiées par des clefs étrangères.
 class Individu(db.Model):
     __tablename__ = "individu"
     id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
@@ -27,18 +35,19 @@ class Individu(db.Model):
     id_autorite = db.Column(db.Text)
     diplome_id = db.Column(db.Integer, db.ForeignKey("diplome.id"))
     these_enc_id = db.Column(db.Integer, db.ForeignKey("these_enc.id"))
-    occupation_id = db.Column(db.Integer, db.ForeignKey("occupation.id"))
+    #occupation_id = db.Column(db.Integer, db.ForeignKey("occupation.id")) CB : cette ligne n'existe plus dans notre modèle de données
     domaine_activite_id = db.Column(db.Integer, db.ForeignKey("domaine_activite.id"))
     distinction_id = db.Column(db.Integer, db.ForeignKey("distinction.id"))
     annee_naissance = db.Column(db.Integer)
     annee_mort = db.Column(db.Integer)
     pays_nationalite = db.relationship("Pays_nationalite", back_populates="individu")
-    diplome = db.relationship ("Diplome", back_populates="individu")
+    diplome = db.relationship("Diplome", back_populates="individu")
     these_enc = db.relationship("These_enc", back_populates="individu")
-    occupation = db.relationship(
-        "Occupation",
-        secondary=avoir_occupation,
-        back_populates="individuals")
+    #occupation_ = db.relationship(
+       # "Occupation",
+        #secondary="Avoir_occupation",
+        #back_populates="individuals") #Méthode SQL Alchemy
+    avoir_occupation = db.relationship("Avoir_occupation", back_populates="individus")
     domaine_activite = db.relationship("Domaine_activite", back_populates="individu")
     distinction = db.relationship("Distinction", back_populates="individu")
 
@@ -59,10 +68,11 @@ class Occupation(db.Model):
     __tablename__ = "occupation"
     id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
     occupation_label = db.Column(db.Text)
-    individu = db.relationship(
-        "Individu",
-        secondary=avoir_occupation,
-        back_populates="occupations_") # La collection a été nommée 'occupations_' pour ne pas que cela crée un conflit dans la fonction 'resultats_avances'
+    avoir_occupation = db.relationship("Avoir_occupation", back_populates="occupations")
+    #individuals = db.relationship(
+        #"Individu",
+        #secondary="Avoir_occupation", # CB : la valeur de back-populages ne devrait pas être occupation ?
+        #back_populates="occupations_") # La collection a été nommée 'occupations_' pour ne pas que cela crée un conflit dans la fonction 'resultats_avances'
                                        # Dans la mesure où une variable 'occupations' a déjà été définie comme mot-clé pour la recherche avancée
 
 # Table contenant les champs disciplinaires (ex : moyen-âge, histoire du livre)
