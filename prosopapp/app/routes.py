64a0f34bdc26e942e5c5069cf6422b2d4fc8fa4f
cabesse <44814764+cabesse@users.noreply.github.com>
2019-04-15 16:10:19 +0200

    Update routes.py

--- prosopapp/app/routes.py
+++ prosopapp/app/routes.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-from flask import render_template
-#cette commande nous permet de relier nos templates à nos urls - routes.
-=======
 from flask import render_template, url_for, request, send_file, redirect
 # L'import de render_template nous permet de relier nos templates à nos urls - routes
 # L'import de url_for permet de construire des URL vers les fonctions et les pages html
@@ -27,39 +23,9 @@ import random
 
 # Les commandes suivantes nous permettent de créer différentes routes - qui correspondent à l'URL des différents pages
 # de notre application :
->>>>>>> ddcde9899bd9e00549db8514b99156eebe50bcdb
 
-from app.app import app
-#Cette commande permet d'importer de notre package app, la variable app, qui instancie notre application.
-
-#Les commandes suivantes nous permettent de créer différentes routes - qui correspondent à l'URL des différents pages
-# de notre application :
 @app.route('/')
 def accueil ():
-<<<<<<< HEAD
-    return render_template("pages/accueil.html")
-#la fonction render_template prend comme premier argument le chemin du template et en deuxième des arguments nommés, qui
-#peuvent ensuite être réutilisé en tant que variables dans les templates.
-
-@app.route('/chercheurs')
-def chercheurs():
-    return render_template("pages/chercheurs.html")
-
-@app.route('/recherche')
-def recherche():
-    return render_template("pages/recherche.html")
-
-@app.route('/resultats')
-def resultat():
-    return render_template("pages/resultats.html")
-#cette route correspond à la page qui affichera les notices abrégées des résultats
-# à voir si on choisit de la conserver sous la dénomination résultat où si l'on préfère un nom qui reprend les mots-clés?
-@app.route('/<string:nomchercheur>')
-def nomchercheur():
-    return render_template("pages/accueil.html")
-#idealement nomchercheur est remplacé par le nomprenom du chercheur
-#cette page correspond à la notice complète sur le chercheur
-=======
     return render_template("pages/accueil.html", title="accueil")
     # La fonction render_template prend comme premier argument le chemin du template et en deuxième des arguments nommés, qui
     # peuvent ensuite être réutilisés en tant que variables dans les templates.
@@ -354,4 +320,4 @@ def download():
     """Route permettant à l'utilisateur de télécharger le fichier prosopochartes.sqlite (base de données sur laquelle se base l'application)"""
     f = '../prosopochartes.sqlite'
     return send_file(f, attachment_filename='prosopochartes.sqlite', as_attachment=True)
->>>>>>> ddcde9899bd9e00549db8514b99156eebe50bcdb
+
