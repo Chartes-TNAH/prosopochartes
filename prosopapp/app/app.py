bfd858a3ec5d0956675e3797ca35ea08606b8d75
cabesse <44814764+cabesse@users.noreply.github.com>
2019-04-15 16:03:43 +0200

    Update app.py

--- prosopapp/app/app.py
+++ prosopapp/app/app.py
@@ -1,23 +1,4 @@
 from flask import Flask
-<<<<<<< HEAD
-
-#import de Flask depuis la librairie flask
-
-app = Flask(__name__)
-
-#Selon le manuel de Grinberg, _name_ est une variable Python prédéfinie qui prend le nom du module dans lequel elle est
-#utilisée. Nous avions vu en cours qu'il pouvait être judicieux de lui donner un nom distinct dans le cas du développement
-#de plusieurs applications tournant sur le même serveur. Comme dans notre cas nous en développons une, nous choisissons de
-#suivre les préconisations de Grinberg.
-
-# app est ici une variable définie comme appartenant à la classe Flask, à ne pas confondre avec notre package app (initialisé
-#avec notre fichier __init__.py.) et qui contient notre application.
-
-from app import routes
-
-#Cette commande permet de relier nos routes - urls à notre application. Elle se situe en dernier car routes a besoin de
-# notre variable app pour fonctionner. Ceci permet d'éviter des erreurs de code.
-=======
 # Import de Flask depuis la librairie flask
 
 import os
@@ -73,4 +54,4 @@ from .errorhandler import page_introuvable, page_supprimee, probleme_serveur
 # Cette commande permet de relier nos routes - urls à notre application. Elle se situe en dernier car routes a besoin de
 # notre variable app pour fonctionner. Ceci permet d'éviter des erreurs de code.
 # Sur le même modèle, nous relions également les fonctions qui gèrent nos erreurs à notre application.
->>>>>>> ddcde9899bd9e00549db8514b99156eebe50bcdb
+
